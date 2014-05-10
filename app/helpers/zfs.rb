a1669d3a4e913d5de5cf3e44f10ab3467e11dd89
Julien Ramseier <julien@ramseier.me>
2014-05-10 11:29:02 +0200

    Show percentage of pool used

--- app/helpers/zfs.rb
+++ app/helpers/zfs.rb
@@ -3,14 +3,18 @@ module ZFS::Helpers
   def zfs_tree(root = [])
     html = ''
 
-    root.children.each do |fs|
+    root.children({recursive: true}).each do |fs|
       html << partial(:'sidebar/fs', locals: {fs: fs})
 
       # Recurse through children
-      html << zfs_tree(fs)
+      #html << zfs_tree(fs)
     end
 
     return html
+  end
+
+  def percentage_used(fs)
+    ((fs.used.to_f / fs.available.to_f) * 100.0).ceil
   end
 
 end
